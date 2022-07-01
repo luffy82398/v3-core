f1c8890d9f2851c4f83819cfbfdec83b219abdfb
luffy82398 <wswzh2123@gmail.com>
2022-07-01 10:50:12 +0000

    独有的数学运算方式

--- test/BitMath.spec.ts
+++ test/BitMath.spec.ts
@@ -15,65 +15,73 @@ describe('BitMath', () => {
     bitMath = await waffle.loadFixture(fixture)
   })
 
-  describe('#mostSignificantBit', () => {
-    it('0', async () => {
-      await expect(bitMath.mostSignificantBit(0)).to.be.reverted
-    })
-    it('1', async () => {
-      expect(await bitMath.mostSignificantBit(1)).to.eq(0)
-    })
-    it('2', async () => {
-      expect(await bitMath.mostSignificantBit(2)).to.eq(1)
-    })
-    it('all powers of 2', async () => {
-      const results = await Promise.all(
-        [...Array(255)].map((_, i) => bitMath.mostSignificantBit(BigNumber.from(2).pow(i)))
-      )
-      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
-    })
-    it('uint256(-1)', async () => {
-      expect(await bitMath.mostSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(255)
-    })
+  // describe('#mostSignificantBit', () => {
+  //   it('0', async () => {
+  //     await expect(bitMath.mostSignificantBit(0)).to.be.reverted
+  //   })
+  //   it('1', async () => {
+  //     expect(await bitMath.mostSignificantBit(1)).to.eq(0)
+  //   })
+  //   it('2', async () => {
+  //     expect(await bitMath.mostSignificantBit(2)).to.eq(1)
+  //   })
+  //   it('all powers of 2', async () => {
+  //     const results = await Promise.all(
+  //       [...Array(255)].map((_, i) => bitMath.mostSignificantBit(BigNumber.from(2).pow(i)))
+  //     )
+  //     // console.log( await Promise.all(
+  //     //   [...Array(10)].map((_, i) => bitMath.mostSignificantBit(BigNumber.from(2).pow(i)))
+  //     // ));
+  //     // console.log([...Array(10)].map((_, i) => i));
+  //     expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
+  //   })
+  //   it('uint256(-1)', async () => {
+  //     expect(await bitMath.mostSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(255) // 运算结果还需要是正数
+  //   })
 
-    it('gas cost of smaller number', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(3568)))
-    })
-    it('gas cost of max uint128', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(2).pow(128).sub(1)))
-    })
-    it('gas cost of max uint256', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(2).pow(256).sub(1)))
-    })
-  })
+  //   it('gas cost of smaller number', async () => {
+  //     // console.log((BigNumber.from(3568))); // 数字转成 BigNumber
+  //     // console.log((await bitMath.getGasCostOfMostSignificantBit(BigNumber.from(3568))).toString()); // 算出花费的 gas
+  //     await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(3568)))
+  //   })
+  //   it('gas cost of max uint128', async () => {
+  //     // console.log((await bitMath.getGasCostOfMostSignificantBit(BigNumber.from(2).pow(128).sub(1))).toString()); // 算出花费的 gas
+  //     await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(2).pow(128).sub(1)))
+  //   })
+  //   it('gas cost of max uint256', async () => {
+  //     await snapshotGasCost(bitMath.getGasCostOfMostSignificantBit(BigNumber.from(2).pow(256).sub(1)))
+  //   })
+  // })
 
-  describe('#leastSignificantBit', () => {
-    it('0', async () => {
-      await expect(bitMath.leastSignificantBit(0)).to.be.reverted
-    })
-    it('1', async () => {
-      expect(await bitMath.leastSignificantBit(1)).to.eq(0)
-    })
-    it('2', async () => {
-      expect(await bitMath.leastSignificantBit(2)).to.eq(1)
-    })
-    it('all powers of 2', async () => {
-      const results = await Promise.all(
-        [...Array(255)].map((_, i) => bitMath.leastSignificantBit(BigNumber.from(2).pow(i)))
-      )
-      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
-    })
-    it('uint256(-1)', async () => {
-      expect(await bitMath.leastSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(0)
-    })
+  // describe('#leastSignificantBit', () => {
+  //   it('0', async () => {
+  //     await expect(bitMath.leastSignificantBit(0)).to.be.reverted
+  //   })
+  //   it('1', async () => {
+  //     expect(await bitMath.leastSignificantBit(1)).to.eq(0)
+  //   })
+  //   it('2', async () => {
+  //     expect(await bitMath.leastSignificantBit(2)).to.eq(1)
+  //   })
+  //   it('all powers of 2', async () => {
+  //     const results = await Promise.all(
+  //       [...Array(255)].map((_, i) => bitMath.leastSignificantBit(BigNumber.from(2).pow(i)))
+  //     )
+  //     expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
+  //     // console.log([...Array(255)].map((_, i) => i));
+  //   })
+  //   it('uint256(-1)', async () => {
+  //     expect(await bitMath.leastSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(0)
+  //   })
 
-    it('gas cost of smaller number', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(3568)))
-    })
-    it('gas cost of max uint128', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(2).pow(128).sub(1)))
-    })
-    it('gas cost of max uint256', async () => {
-      await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(2).pow(256).sub(1)))
-    })
-  })
+  //   it('gas cost of smaller number', async () => {
+  //     await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(3568)))
+  //   })
+  //   it('gas cost of max uint128', async () => {
+  //     await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(2).pow(128).sub(1)))
+  //   })
+  //   it('gas cost of max uint256', async () => {
+  //     await snapshotGasCost(bitMath.getGasCostOfLeastSignificantBit(BigNumber.from(2).pow(256).sub(1)))
+  //   })
+  // })
 })
